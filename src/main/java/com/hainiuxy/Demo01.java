e06af834e27ea2bd2627031aae5a53426749856f
fightingguowei <gwupward@163.com>
2023-09-21 22:49:49 +0800

    第一次提交

---
+++ src/main/java/com/hainiuxy/Demo01.java
@@ -0,0 +1,10 @@
+package com.hainiuxy;
+
+public class Demo01 {
+    public static void main(String[] args) {
+        System.out.println(222);
+        System.out.println(333);
+        System.out.println(444);
+        System.out.println(555);
+    }
+}
