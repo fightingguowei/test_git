25f542abccb1a26282d2affa9633efd8cb82b25b
fightingguowei <gwupward@163.com>
2023-09-21 23:13:02 +0800

    第五次提交

--- src/main/java/com/hainiuxy/Demo01.java
+++ src/main/java/com/hainiuxy/Demo01.java
@@ -16,6 +16,11 @@ public class Demo01 {
         System.out.println(555);
         System.out.println(555);
         System.out.println(555);
+        System.out.println(555);
+        System.out.println(555);
+        System.out.println(555);
+        System.out.println(555);
+        System.out.println(555);
 
 
     }
